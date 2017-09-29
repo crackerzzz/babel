a84015bd3710341da0c19e34737f7f9cd6e6aea7
crackerzzz <shreejwal.shrestha@gmail.com>
2017-09-29 11:08:17 +1000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + "/app",
+    entry: "./js/app.js",
+    output: {
+        filename: "app.js",
+        path: __dirname + "/dist"
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loaders: ["babel-loader"]
+        }]
+    }
+}
